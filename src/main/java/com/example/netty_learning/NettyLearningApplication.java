c67bb23b80842c619d4051dbc67c05727a4589e2
loloassange <loloassange@163.com>
2019-05-14 10:19:31 +0800

    win_o

---
+++ src/main/java/com/example/netty_learning/NettyLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.netty_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyLearningApplication.class, args);
+    }
+
+}
